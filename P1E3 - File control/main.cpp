ef1540d333bcbf8ab626f4d6c6008da384eacf1c
Martin <martinek@rychly.cz>
2025-04-08 22:16:22 +0200

    Uploaded correct version of P1E3 - File control

--- P1E3 - File control/main.cpp
+++ P1E3 - File control/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <stdlib.h>
+#include <string>
+#include <ctime>
 #include <fstream>
 
-#define quotes 20;
+#define quotes 10
 
 using namespace std;
 
@@ -10,19 +12,22 @@ int main(){
     int targetLine;
     unsigned seed = static_cast<unsigned>(time(0));
     string line;
+
     srand(seed);
+
     do{
-        ifstream file("quotes.txt", ios::in | ios::binary);
-        if (!file) {
-            cerr << "Error opening file" << endl;
+        ifstream file("quotes.txt");
+        if (!file.is_open()) {
+            cerr << "Error opening file." << endl;
             return 1;
         }
+
         targetLine = rand() % quotes + 1;
-        for(int i = 1; i < targetLine; i++){
+        for(int i = 0; i < targetLine; i++){
             getline(file, line);
         }
         file.close();
     }while(line == "");
-    cout << line << endl;
+    cout << "Tux the wise: " << line << endl;
     return 0;
 }
